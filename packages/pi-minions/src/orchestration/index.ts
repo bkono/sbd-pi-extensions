b91891fe5ecb9bfa64ab642a48f4832b58b743ef
Bryan Konowitz <bryan@kono.sh>
2026-08-28 17:10:23 -0700

    feat(pi-minions): enforce one open orchestration group and immutable cwd

---
+++ packages/pi-minions/src/orchestration/index.ts
@@ -0,0 +1,9 @@
+export {
+  GROUP_REJECT_REASONS,
+  type GroupRejectReason,
+  isResolveGroupReject,
+  type OpenOrchestrationGroup,
+  OrchestrationGroupState,
+  type ResolveGroupInput,
+  type ResolveGroupResult,
+} from "./group-state.js";
